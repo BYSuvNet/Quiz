b79a5c2928a0a6523c756e6cd9b0f8f0120b6306
Gus <gusgit@fastmail.org>
2023-10-02 12:41:09 +0200

    Refactor to split UI into separate class

--- Program.cs
+++ Program.cs
@@ -1,61 +1,9 @@
-﻿using System.Text.Json;
-
-class Program
+﻿class Program
 {
     static void Main()
     {
         QuestionsManager questionsManager = new("questions.json");
-
-        Console.WriteLine("Välkommen till Quizet!");
-        Console.WriteLine($"Det består av {questionsManager.NumberOfQuestions} frågor och maxpoängen är {questionsManager.MaxPoints} poäng");
-
-        while (true)
-        {
-            List<Question> failedQuestions = new();
-
-            int poäng = 0;
-
-            foreach (Question fråga in questionsManager.GetAllQuestions())
-            {
-                int pointsForQuestion = AskQuestions(fråga);
-                if (pointsForQuestion == 0)
-                {
-                    failedQuestions.Add(fråga);
-                }
-                else
-                {
-                    poäng += pointsForQuestion;
-                }
-            }
-
-            Console.WriteLine($"Nu är quizet klart. Du fick {poäng} av max {questionsManager.MaxPoints} poäng.");
-            Console.WriteLine($"Du missade {failedQuestions.Count} frågor.");
-            Console.WriteLine("Vill du göra om quizet? (j/n)");
-
-            if (Console.ReadLine() == "n")
-            {
-                break;
-            }
-        }
-
-        Console.WriteLine("Tack för ditt quizzande!");
+        QuestionsUI ui = new(questionsManager);
+        ui.Run();
     }
-
-    static int AskQuestions(Question question)
-    {
-        Console.WriteLine(question.Text);
-
-        if (question.CheckAnswer(Console.ReadLine()))
-        {
-            Console.WriteLine("Rätt!");
-            return question.Points;
-        }
-        else
-        {
-            Console.WriteLine("Fel! Rätt svar är: " + question.Answer);
-        }
-
-        return 0;
-    }
-
 }
---
+++ QuestionsUI.cs
@@ -0,0 +1,59 @@
+class QuestionsUI
+{
+    QuestionsManager questionsManager;
+    public QuestionsUI(QuestionsManager questionsManager) => this.questionsManager = questionsManager;
+
+    public void Run()
+    {
+        Console.WriteLine("Välkommen till Quizet!");
+        Console.WriteLine($"Det består av {questionsManager.NumberOfQuestions} frågor och maxpoängen är {questionsManager.MaxPoints} poäng");
+
+        while (true)
+        {
+            List<Question> failedQuestions = new();
+
+            int poäng = 0;
+
+            foreach (Question fråga in questionsManager.GetAllQuestions())
+            {
+                int pointsForQuestion = AskQuestions(fråga);
+                if (pointsForQuestion == 0)
+                {
+                    failedQuestions.Add(fråga);
+                }
+                else
+                {
+                    poäng += pointsForQuestion;
+                }
+            }
+
+            Console.WriteLine($"Nu är quizet klart. Du fick {poäng} av max {questionsManager.MaxPoints} poäng.");
+            Console.WriteLine($"Du missade {failedQuestions.Count} frågor.");
+            Console.WriteLine("Vill du göra om quizet? (j/n)");
+
+            if (Console.ReadLine() == "n")
+            {
+                break;
+            }
+        }
+
+        Console.WriteLine("Tack för ditt quizzande!");
+    }
+
+    int AskQuestions(Question question)
+    {
+        Console.WriteLine(question.Text);
+
+        if (question.CheckAnswer(Console.ReadLine()))
+        {
+            Console.WriteLine("Rätt!");
+            return question.Points;
+        }
+        else
+        {
+            Console.WriteLine("Fel! Rätt svar är: " + question.Answer);
+        }
+
+        return 0;
+    }
+}
